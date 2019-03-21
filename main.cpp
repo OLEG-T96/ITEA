cf5098914fa0ec091a4d5283d621b4ce8b31c0e4
OLEG-T96 <48726248+OLEG-T96@users.noreply.github.com>
2019-03-22 01:55:27 +0200

    Tree

--- main.cpp
+++ main.cpp
@@ -1,54 +1,36 @@
 #include <QCoreApplication>
 #include <iostream>
-#include <cmath>
-
 using std::cout;
 using std::cin;
 using std::endl;
 
-
-const int coef_2 =2;
-const int coef_4 =4;
-
-int a,b,c,D;
-
-
+int blocks;
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication app(argc, argv);
+   // QCoreApplication a(argc, argv);
 
+    cout<<"Input number of blocks ";
+    cin>>blocks;
+    int stars,space;
+    int counter =0;
+    stars = (blocks*2-1);
+    space = ((stars-1)/2);
+    for(int i =0;i<=stars;i+=2 )
+      {
+        for(int j=(space-counter);j>0;j--)
+           {
+             cout<<" ";
+           }
+        for(int x=0;x<=i;x++)
+           {
+             cout<<"*";
+           }
+        cout<<endl;
+        counter++;
 
-    cout<<"a = ";
-    cin>>a;
-      if(a == 0)
-        {
-         cout<<endl<<"a cannot be  0";
-         return 0;
-        }
-    cout<<endl<<"b = ";
-    cin>>b;
-    cout<<endl<<"c = ";
-    cin>>c;
-
-    D =(pow(b,2)-4*a*c);
-
-    if(D > 0 )
-        {
-          cout<<endl<<"x1 = "<<((-b + sqrt(D))/2*a);
-          cout<<endl<<"x1 = "<<((-b - sqrt(D))/2*a);
-        }
-    else if(D == 0)
-        {
-          cout<<endl<<"x1 = "<<(-b/2*a);
-        }
-    else
-        {
-           cout<<endl<<"D<0";
-           return 0;
-        }
-
+      }
 
    return 0;
-   // return app.exec();
+   // return a.exec();
 }
